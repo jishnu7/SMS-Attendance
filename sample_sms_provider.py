4293309c8f9eb593ea65db7034644aacec3c6c45
Jishnu <jishnu7@gmail.com>
2012-01-07 13:00:40 +0530

    clean sample code

--- sample_sms_provider.py
+++ sample_sms_provider.py
@@ -1,19 +1,18 @@
-import cookielib
-import urllib2
-
 
 class sample_sms_provider():
     def __init__(self):
         # Set Cookie
-        sms_cookie = cookielib.CookieJar()
-        self.sms_opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(sms_cookie))
-        self.sms_opener.addheaders.append(('User-agent', 'Mozilla/5.0')).
+        print "Setting cookie
 
     def login(self, username, password):
         # Login
         print "Login : ", username, password
+        # return false if login is incorrect
+        return True
 
     # Function to send SMS
     def send(self, message, mobile_num) :
         # success message
         print "Send message :", mobile_num, message
+        # return false if sending failed
+        return True
